c9e25bdcccbd560712dc2bc448951f89a69b169b
AlexisUFG <ia.alexismartinez@ufg.edu.sv>
2024-11-08 23:18:50 -0600

    :tada: add Fluent Validations Nuget, implement in proyect and create users valdiations

--- src/UsersCrudWithFluentValidations/Program.cs
+++ src/UsersCrudWithFluentValidations/Program.cs
@@ -1,4 +1,7 @@
 
+using FluentValidation;
+using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
+
 namespace UsersCrudWithFluentValidations
 {
     public class Program
@@ -13,6 +16,9 @@ namespace UsersCrudWithFluentValidations
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+            builder.Services.AddFluentValidationAutoValidation();
 
             var app = builder.Build();
 
---
+++ src/UsersCrudWithFluentValidations/Validations/UserValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using UsersCrudWithFluentValidations.Models;
+
+namespace UsersCrudWithFluentValidations.Validations
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("The {PropertyName} field should not be empty.");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("The {PropertyName} field should not be empty.");
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("The {PropertyName} field should not be empty.");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("The {PropertyName} field should not be empty.");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("The {PropertyName} field is not a valid email address.");
+            RuleFor(x => x.LastModifiedDate).Must((user, _) =>
+            {
+                return user.CreationDate < user.LastModifiedDate;
+            }).WithMessage("The {PropertyName} field is not a valid email address.");
+        }
+    }
+}
